32882347e82a21e56a0973b0ce6730a5214b7851
cecemyys <cecemyy@gmail.com>
2023-04-19 17:31:39 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const searchform = document.querySelector(".search-form");
+
+//! buttons
+const searchBtn = document.guerySelector("#search-btn");
+
+searchBtn.addEventListener("click", function (){
+    searchForm.classList.toggle("active");
+})
